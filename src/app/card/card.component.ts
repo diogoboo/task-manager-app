866cd10a0a78e7802da22966077616863ffa4f1c
diogoboo <diogoboo@gmail.com>
2023-09-19 14:00:51 +0100

    Last Changes related Card Component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from '../environment';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +8,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  projects: any[] = [];
+  newProject = '';
+  tasks: { [projectId: number]: any[] } = {};
+  newTask = '';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadProjects();
+  }
+
+  loadProjects() {
+    this.http.get<string[]>(`${environment.apiBaseUrl}/projects`).subscribe((data) => {
+      this.projects = data;
+
+      this.projects.forEach((project) => {
+        this.loadTasks(project.projectId);
+      });
+    });
+  }
+
+  loadTasks(projectId: number) {
+    this.http.get<string[]>(`${environment.apiBaseUrl}/tasks/${projectId}`).subscribe((data) => {
+      this.tasks[projectId] = data; 
+    });
+  }
+
+  addProject() {
+    if (this.newProject.trim() !== '') {
+      this.http.post(`${environment.apiBaseUrl}/projects`, { description: this.newProject }).subscribe(() => {
+        this.loadProjects(); 
+        this.newProject = '';
+      });
+    }
+  }
+
+  addTask(projectId: number) {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${environment.token}`
+    });
+
+    if (this.newTask.trim() !== '') {
+      this.http.post(`${environment.apiBaseUrl}/tasks/${projectId}`, { description: this.newTask }, { headers }).subscribe(() => {
+        this.loadTasks(projectId); 
+        this.newTask = '';
+      });
+    }
+  }
+
+  removeProject(projectId: number) {
+    this.http.delete(`${environment.apiBaseUrl}/projects/${projectId}`).subscribe(() => {
+      this.projects = this.projects.filter(project => project.projectId !== projectId);
+      this.loadProjects(); 
+    });
+  }
+
+  removeTask(projectId: number, taskId: number) {
+    this.http.delete(`${environment.apiBaseUrl}/tasks/${taskId}`).subscribe(() => {
+      this.loadTasks(projectId); 
+    });
+  }
 }
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../environment';
 @Component({
   selector: 'app-task',
@@ -7,33 +7,39 @@ import { environment } from '../environment';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  @Input() projectId: any;
+  
   tasks: any[] = [];
   newTask = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    // Load tasks from the backend when the component initializes
     this.loadTasks();
   }
 
   loadTasks() {
-    this.http.get<string[]>(`${environment.apiBaseUrl}/tasks`).subscribe((data) => {
+    this.http.get<string[]>(`${environment.apiBaseUrl}/tasks/${this.projectId}`).subscribe((data) => {
       this.tasks = data;
     });
   }
 
+
   addTask() {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${environment.token}`
+    });
+
     if (this.newTask.trim() !== '') {
-      this.http.post(`${environment.apiBaseUrl}/tasks`, { description: this.newTask }).subscribe(() => {
-        this.loadTasks(); // Reload tasks after adding a new one
+      this.http.post(`${environment.apiBaseUrl}/tasks/${this.projectId}`, { description: this.newTask }, { headers }).subscribe(() => {
+        this.loadTasks(); 
         this.newTask = '';
       });
     }
   }
 
-  removeTask(taskName: string) {
-    this.http.delete(`${environment.apiBaseUrl}/tasks/${taskName}`).subscribe(() => {
+  removeTask(taskId: number) {
+    this.http.delete(`${environment.apiBaseUrl}/tasks/${taskId}`).subscribe(() => {
       this.loadTasks(); // Reload tasks after removing one
     });
   }
